ac86dff3549457d865fd132ed713eabd452887c6
SoLomiaDav <92159435+SoLomiaDav@users.noreply.github.com>
2022-06-14 15:11:05 +0300

    home

---
+++ Shop/Shop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Shop.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllBooks _bookRep;
+        
+
+        public HomeController(IAllBooks bookRep)
+        {
+            _bookRep = bookRep;           
+        }
+
+        public ViewResult Index()
+        {
+            var homeBooks = new HomeViewModel
+            {
+                favBooks = _bookRep.getFavBooks
+            };
+            return View(homeBooks);
+        }
+    }
+}
